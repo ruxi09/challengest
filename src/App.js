c55a2412740384f85b8e5624c7090dfb5756e2a1
Cosmin <minut1@yahoo.com>
2020-02-29 16:27:56 +0000

    Implement Room, Chat and ChatMessage

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Button from '@material-ui/core/Button';
-import CircularProgress from '@material-ui/core/CircularProgress';
-import TextField from '@material-ui/core/TextField';
+import {AppBar, Toolbar, Button, Card, CircularProgress, TextField, Typography, IconButton} from '@material-ui/core'
+import ArrowBackIcon from '@material-ui/icons/ArrowBack';
+import SendIcon from '@material-ui/icons/Send';
+import Paper from "@material-ui/core/Paper";
 
 function App() {
-  const [page, setPage] = useState("home");
+  const [page, setPage] = useState("room");
   const [roomId, setRoomId] = useState("");
+  const [teamId, setTeamId] = useState("");
 
   return (
     <div className="App">
       <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"/>
       {page === "home" ?
-        <Home goToRoom={(roomId) => {
+        <Home goToRoom={(roomId, teamId) => {
           setRoomId(roomId);
+          setTeamId(teamId);
           setPage("room");
         }}/> :
-        <Room roomId={roomId} goToRoom={() => setPage("home")}/>}
+        <Room roomId={roomId} teamId={teamId} goToRoom={() => setPage("home")}/>}
     </div>
   );
 }
@@ -34,7 +37,7 @@ function Home({goToRoom}) {
   return (
     <div className="App">
       <header className="Home-Container">
-        <h className="Title">Challengest</h>
+        <Typography variant="h1" className="Title">Challengest</Typography>
 
         <img src={logo} className="App-logo" alt="logo"/>
 
@@ -47,7 +50,7 @@ function Home({goToRoom}) {
 
             timer.current = setTimeout(() => {
               setCreateLoading(false);
-              goToRoom("new")
+              goToRoom("new", "owner");
             }, 3000);
           }}
           style={{marginBottom: 16}}>
@@ -68,7 +71,7 @@ function Home({goToRoom}) {
 
                 timer.current = setTimeout(() => {
                   setJoinLoading(false);
-                  goToRoom(roomId)
+                  goToRoom(roomId, "participant");
                 }, 3000)
               }
             }}
@@ -92,20 +95,6 @@ function Home({goToRoom}) {
   );
 }
 
-function Room({roomId, goToRoom}) {
-  document.title = `Room - ${roomId}`;
-
-  return (
-    <div className="Room-Container">
-      <h>Welcome to {roomId}</h>
-      <div>
-        <Button onClick={() => goToRoom()} variant="contained">
-          Back
-        </Button>
-      </div>
-    </div>);
-}
-
 function LoadingButton({variant, color, loading, onClick, style, children}) {
   return (
     <div className="LoadingButton-Container">
@@ -117,6 +106,156 @@ function LoadingButton({variant, color, loading, onClick, style, children}) {
       }
     </div>
   )
+}
+
+function Room({roomId, teamId, goToRoom}) {
+  const [messages, setMessages] = useState([
+    {text: "alo pronto", teamId: "owner", timeStamp: "12:34"},
+    {text: "sunt contactu", teamId: "participant", timeStamp: "45:67"}, {
+      text: "alo pronto",
+      teamId: "owner",
+      timeStamp: "12:34"
+    },
+    {text: "sunt contactu", teamId: "participant", timeStamp: "45:67"}, {
+      text: "alo pronto",
+      teamId: "owner",
+      timeStamp: "12:34"
+    },
+    {text: "sunt contactu", teamId: "participant", timeStamp: "45:67"}, {
+      text: "alo pronto",
+      teamId: "owner",
+      timeStamp: "12:34"
+    },
+    {text: "sunt contactu", teamId: "participant", timeStamp: "45:67"}, {
+      text: "alo pronto",
+      teamId: "owner",
+      timeStamp: "12:34"
+    },
+    {text: "sunt contactu", teamId: "participant", timeStamp: "45:67"}, {
+      text: "alo pronto",
+      teamId: "owner",
+      timeStamp: "12:34"
+    },
+    {text: "sunt contactu", teamId: "participant", timeStamp: "45:67"}, {
+      text: "alo pronto",
+      teamId: "owner",
+      timeStamp: "12:34"
+    },
+    {text: "sunt contactu", teamId: "participant", timeStamp: "45:67"}, {
+      text: "alo pronto",
+      teamId: "owner",
+      timeStamp: "12:34"
+    },
+    {text: "sunt contactu", teamId: "participant", timeStamp: "45:67"}, {
+      text: "alo pronto",
+      teamId: "owner",
+      timeStamp: "12:34"
+    },
+    {text: "sunt contactu", teamId: "participant", timeStamp: "45:67"}, {
+      text: "alo pronto",
+      teamId: "owner",
+      timeStamp: "12:34"
+    },
+    {text: "sunt contactu", teamId: "participant", timeStamp: "45:67"}, {
+      text: "alo pronto",
+      teamId: "owner",
+      timeStamp: "12:34"
+    },
+    {text: "sunt contactu", teamId: "participant", timeStamp: "45:67"}, {
+      text: "alo pronto",
+      teamId: "owner",
+      timeStamp: "12:34"
+    },
+    {text: "sunt contactu", teamId: "participant", timeStamp: "45:67"}, {
+      text: "alo prontoalo prontoalo prontoalo prontoalo prontoalo prontoalo prontoalo prontoalo prontoalo prontoalo prontoalo prontoalo prontoalo prontoalo prontoalo prontoalo prontoalo prontoalo prontoalo prontoalo prontoalo prontoalo prontoalo prontoalo prontoalo prontoalo prontoalo prontoalo prontoalo prontoalo prontoalo prontoalo prontoalo prontoalo prontoalo prontoalo prontoalo prontoalo prontoalo prontoalo prontoalo prontoalo prontoalo prontoalo prontoalo prontoalo prontoalo pronto",
+      teamId: "owner",
+      timeStamp: "12:34"
+    },
+    {
+      text: "sunt contactusunt contactusunt contactusunt contactusunt contactusunt contactusunt contactusunt contactusunt contactusunt contactusunt contactusunt contactusunt contactusunt contactu  sunt contactusunt contactusunt contactusunt contactusunt contactusunt contactusunt contactusunt contactu",
+      teamId: "participant",
+      timeStamp: "45:67"
+    }, {
+      text: "alo pronto",
+      teamId: "owner",
+      timeStamp: "12:34"
+    },
+    {text: "sunt contactu", teamId: "participant", timeStamp: "45:67"},
+  ]);
+  const [input, setInput] = useState("");
+
+  document.title = `Room - ${roomId}`;
+
+  return (
+    <div className="Room-Container">
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" onClick={() => goToRoom()} style={{marginRight: 16, color: "white"}}>
+            <ArrowBackIcon/>
+          </IconButton>
+
+          <Typography variant="h5">
+            Room {roomId} - Team {teamId}
+          </Typography>
+        </Toolbar>
+      </AppBar>
+
+      <Chat messages={messages}/>
+
+      <div className="LeftRight" style={{paddingLeft: 16, paddingRight: 16}}>
+        <TextField
+          label="Message"
+          value={input}
+          onChange={(event) => setInput(event.target.value)}
+          variant="outlined"
+          onKeyPress={(event) => {
+            if (event.key === 'Enter') {
+              alert(input);
+              setInput("")
+            }
+          }}
+          style={{flex: 1}}/>
+
+        <IconButton
+          onClick={() => {
+            alert(input);
+            setInput("");
+          }}
+          style={{marginLeft: 8}}>
+          <SendIcon/>
+        </IconButton>
+      </div>
+    </div>
+  );
+}
+
+function Chat({messages}) {
+  return (
+    <Paper className="Chat-Wrapper" elevation={5}>
+      <ul className="Chat-Container">
+        {messages.map(ChatMessage)}
+      </ul>
+    </Paper>
+  )
+}
+
+function ChatMessage(message) {
+  const {text, teamId, timeStamp} = message;
+  const alignment = teamId === "owner" ? "left" : "right";
+
+  return (
+    <div className={`ChatMessage-Wrapper ${alignment}`}>
+      <Card className="ChatMessage-Container" style={{textAlign: "left"}}>
+        <Typography variant="body1">
+          {text}
+        </Typography>
+
+        <Typography variant="caption" style={{marginLeft: 16, marginBottom: -4, alignSelf: "flex-end", fontSize: 10}}>
+          {timeStamp}
+        </Typography>
+      </Card>
+    </div>
+  );
 }
 
 export default App;
