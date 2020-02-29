da4a92acca912db52917285343c4dcb2668a3102
Charlie Cowan <charlie.g.cowan@gmail.com>
2020-02-29 18:05:02 +0000

    Added join room

--- src/App.js
+++ src/App.js
@@ -74,11 +74,14 @@ function Home({goToRoom}) {
 
               if (!joinRoomError) {
                 setJoinLoading(true);
-
-                timer.current = setTimeout(() => {
-                  setJoinLoading(false);
-                  goToRoom(roomId, "participant");
-                }, 3000)
+axios({method: 'post', url: 'http://127.0.0.1:3001/joinRoom', headers: {}, data: {teamName, roomId}})
+    .then(function (response) {
+        setJoinLoading(false);
+        goToRoom(roomId, teamName);
+    })
+    .catch(function (error) {
+        console.log(error);
+    });
               }
             }}
             style={{marginRight: 8, background: 'red', marginTop: 8}}>
